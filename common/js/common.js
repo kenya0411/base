8009cbf58145b711e92ddf82ae05ba906dd758d4
unknown <you@example.co>
2020-01-17 18:05:08 +0900

    auto

--- common/js/common.js
+++ common/js/common.js
@@ -200,3 +200,14 @@ if(urlHash) {
 }
 
 });
+
+//Retina-Srcset.js
+$(function(){
+  var retinaCheck = window.devicePixelRatio;
+  if(retinaCheck >= 2) { // Retinaディスプレイのときを分岐させている
+    $('img.retina').each( function() {
+      var retinaimg = $(this).attr('src').replace(/\.(?=(?:png|jpg|jpeg)$)/i, '@2x.');
+      $(this).attr('srcset', retinaimg + " 2x");
+    });
+  }
+});
